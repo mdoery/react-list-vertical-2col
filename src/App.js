e558a7190b913aa333a3b58088ba8f5903942003
Marya Doery <mdoery@gmail.com>
2019-03-29 15:30:13 -0400

    Styles the divs so they wrap, are rounded, have margin and padding.

--- src/App.js
+++ src/App.js
@@ -5,21 +5,28 @@ import './App.css';
 class App extends Component {
   render() {
     return (
+      /**
+       * I want the divs to wrap, as described in this Stack Overflow question:
+       * https://stackoverflow.com/questions/11517415/how-can-i-wrap-divs
+       */
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <span className="roundedItem">Kid Cudi</span>
+        <span className="roundedItem">David Guetta</span>
+        <span className="roundedItem">Rhianna</span>
+        <span className="roundedItem">Skrillex</span>
+        <span className="roundedItem">Martin Garrix</span>
+        <span className="roundedItem">Avicii</span>
+        <span className="roundedItem">Zedd</span>
+        <span className="roundedItem">Diplo</span>
+        <span className="roundedItem">Calvin Harris</span>
+        <span className="roundedItem">Deadmau5</span>
+        <span className="roundedItem">Afrojack</span>
+        <span className="roundedItem">Daft Punk</span>
+        <span className="roundedItem">Porter Robinson</span>
+        <span className="roundedItem">DJ Snake</span>
+        <span className="roundedItem">Swedish House Mafia</span>
+        <span className="roundedItem">Axwell</span>
+        <span className="roundedItem">Tiësto</span>
       </div>
     );
   }
